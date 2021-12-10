518ad031f78590384beaf32b9c8f0ecaa38b926e
Josef Kuchař <josef.kuchar267@gmail.com>
2021-12-10 16:21:12 +0100

    More progress

--- octave/priklad1.m
+++ octave/priklad1.m
@@ -1,4 +1,4 @@
-output_precision(6);
+output_precision(8);
 
 % Zadani
 U1 = 100;
@@ -13,30 +13,30 @@ R7 = 260;
 R8 = 180;
 
 % Postupne zjednodusovani
-U = U1 + U2;
-R45 = (R4 * R5) / (R4 + R5);
-R457 = R45 + R7;
+U = U1 + U2
+R45 = (R4 * R5) / (R4 + R5)
+R457 = R45 + R7
 
 % Transfigurace trojuhelnik -> hvezdu
-RA = (R1 * R2) / (R1 + R2 + R3);
-RB = (R1 * R3) / (R1 + R2 + R3);
-RC = (R2 * R3) / (R1 + R2 + R3);
+RA = (R1 * R2) / (R1 + R2 + R3)
+RB = (R1 * R3) / (R1 + R2 + R3)
+RC = (R2 * R3) / (R1 + R2 + R3)
 
-RB457 = RB + R457;
-RC6 = RC + R6;
-RBC4567 = (RB457 * RC6) / (RB457 + RC6);
+RB457 = RB + R457
+RC6 = RC + R6
+RBC4567 = (RB457 * RC6) / (RB457 + RC6)
 
 % Vysledny Rekv
-Rekv = RA + RBC4567 + R8;
+Rekv = RA + RBC4567 + R8
 
 % Vypocet celkoveho proudu v obvodu
-I = U / Rekv;
+I = U / Rekv
 
 % Zpetne dopocitani proudu a napeti na R7
-UBC4567 = RBC4567 * I;
-IB457 = UBC4567 / RB457;
-IR7 = IB457;
-UR7 = R7 * IR7;
+UBC4567 = RBC4567 * I
+IB457 = UBC4567 / RB457
+IR7 = IB457
+UR7 = R7 * IR7
 
 % Zobrazeni vysledku
 display(IR7);
