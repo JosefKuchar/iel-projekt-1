9fb6b027a408da457a3493ad9907aa6c75c2874c
Josef Kuchař <josef.kuchar267@gmail.com>
2021-12-12 14:08:28 +0100

    Finalize 3,4

--- octave/priklad2.m
+++ octave/priklad2.m
@@ -1,4 +1,4 @@
-output_precision(6);
+output_precision(8);
 
 % Zadani
 U = 200;
@@ -9,19 +9,15 @@ R4 = 240;
 R5 = 450;
 
 % Vypocet Ri
-R45 = R4 + R5;
-R345 = (R3 * R45) / (R3 + R45);
-Ri = (R2 * R345) / (R2 + R345);
+R45 = R4 + R5
+R345 = (R3 * R45) / (R3 + R45)
+Ri = (R2 * R345) / (R2 + R345)
 
 % Vypocet Ui
-Rekv = R345 + R2;
-Ix = U / Rekv;
-Ui = R2 * Ix;
+Rekv = R345 + R2
+Ix = U / Rekv
+Ui = R2 * Ix
 
 % Vypocet pozadovanych velicin
-IR1 = Ui / (Ri + R1);
-UR1 = R1 * IR1;
-
-% Zobrazeni vysledku
-display(IR1)
-display(UR1);
+IR1 = Ui / (Ri + R1)
+UR1 = R1 * IR1
